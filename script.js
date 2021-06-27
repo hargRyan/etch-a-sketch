00009cf5f7c8c009d6af8ac5d58c8596837ee63e
Ryan Hargreaves <hargryan@protonmail.com>
2021-06-27 12:26:33 -0700

    created grid and added hover functionality, next is adding event listener to each .cell

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let grid = document.getElementById('grid');
+//GLOBAL VARIABLES AND FUNCTIONS GO HERE
 
-for(let i=0; i <= 255; i++) {
+function createGrid() {
+    let grid = document.getElementById('grid');
+
+    for(let i=0; i <= 255; i++) {
     
-    let div = document.createElement('div');
-    container.appendChild(div);
+        let div = document.createElement('div');
+        div.classList.add("cell");
+        // addEventListener('mouseover', changeBGcolor);
+        grid.appendChild(div);
+        
+    }
 }
+
+function changeBGcolor (node) {
+    // should set the background color of a given node to a random color.
+    // want this function to run within the for loop.
+
+    
+}
+
+//STATEMENTS GO HERE
+
+createGrid();
